aa4d2f4b9ee369a9a6b74e9c456daa0f7f826f3c
Jay Prakash <jayp7808@gmail.com>
2024-06-18 16:46:17 +0530

    font change

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400 '>
       <div className='w-full block'>
         <Header />
-        <main className='font-bold'>
+        <main >
         Blog  <Outlet />
         </main>
         <Footer />
